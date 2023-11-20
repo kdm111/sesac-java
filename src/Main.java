8d96b41f968b9aad142a805b0a0b86e97dfb17ea
kdm111 <pjsu94@naver.com>
2023-11-20 14:43:42 +0900

    수정

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		System.out.println("github connect");
-		System.out.println("commit test");
-		for (int i = 0; i < 5; i++) {
-			System.out.println(i);
-		}
+		System.out.println("another projects");
 	}
 }
