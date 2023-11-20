9be347c7651b999747a75780c337c7cc4f61622b
kdm111 <pjsu94@naver.com>
2023-11-20 14:40:27 +0900

    새로운 프로젝트 생

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		System.out.println("hello world");
 		System.out.println("github connect");
 		System.out.println("commit test");
-	
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
 	}
-
 }
