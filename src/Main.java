6ba0dc51e0fc694ec5f9ee5a364950af6843f38b
kdm111 <pjsu94@naver.com>
2023-11-20 15:11:36 +0900

    add for i

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		for (int i = 1; i <= 5; i++ ) {
+			System.out.println("i is " + i);
+		}
 	}
 }
